bc6be7ca257b002f1578d980d1f4bf65a9010ee4
Ralf Schandl <rakus@users.noreply.github.com>
2021-10-16 13:49:42 +0200

    Extend test of OrderedProperties

--- src/test/java/de/r3s6/maven/constcreator/OrderedPropertiesTest.java
+++ src/test/java/de/r3s6/maven/constcreator/OrderedPropertiesTest.java
@@ -1,12 +1,17 @@
 package de.r3s6.maven.constcreator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Properties;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.junit.jupiter.api.Test;
 
 class OrderedPropertiesTest {
@@ -31,6 +36,18 @@ class OrderedPropertiesTest {
         assertEquals("test0009", iter.next());
         assertEquals("test0010", iter.next());
 
+        final Enumeration<Object> enumeration = props.keys();
+
+        assertEquals("test0001", enumeration.nextElement());
+        assertEquals("test0002", enumeration.nextElement());
+        assertEquals("test0003", enumeration.nextElement());
+        assertEquals("test0004", enumeration.nextElement());
+        assertEquals("test0005", enumeration.nextElement());
+        assertEquals("test0006", enumeration.nextElement());
+        assertEquals("test0007", enumeration.nextElement());
+        assertEquals("test0008", enumeration.nextElement());
+        assertEquals("test0009", enumeration.nextElement());
+        assertEquals("test0010", enumeration.nextElement());
     }
 
     @Test
@@ -53,6 +70,87 @@ class OrderedPropertiesTest {
         assertEquals("test0009", iter.next());
         assertEquals("test0010", iter.next());
 
+        final Enumeration<Object> enumeration = props.keys();
+
+        assertEquals("test0001", enumeration.nextElement());
+        assertEquals("test0002", enumeration.nextElement());
+        assertEquals("test0003", enumeration.nextElement());
+        assertEquals("test0004", enumeration.nextElement());
+        assertEquals("test0005", enumeration.nextElement());
+        assertEquals("test0006", enumeration.nextElement());
+        assertEquals("test0007", enumeration.nextElement());
+        assertEquals("test0008", enumeration.nextElement());
+        assertEquals("test0009", enumeration.nextElement());
+        assertEquals("test0010", enumeration.nextElement());
     }
 
+    @Test
+    void testRemove() throws IOException {
+        final Properties props = new OrderedProperties();
+        try (final InputStream stream = OrderedPropertiesTest.class.getResourceAsStream("/test-props.properties")) {
+            props.load(stream);
+        }
+
+        props.remove("test00010");
+
+        final Iterator<Object> iter = props.keySet().iterator();
+
+        assertEquals("test0001", iter.next());
+        assertEquals("test0002", iter.next());
+        assertEquals("test0003", iter.next());
+        assertEquals("test0004", iter.next());
+        assertEquals("test0005", iter.next());
+        assertEquals("test0006", iter.next());
+        assertEquals("test0007", iter.next());
+        assertEquals("test0008", iter.next());
+        assertEquals("test0009", iter.next());
+
+    }
+
+    @Test
+    void testRemoveWithValue() throws IOException {
+        final Properties props = new OrderedProperties();
+        try (final InputStream stream = OrderedPropertiesTest.class.getResourceAsStream("/test-props.properties")) {
+            props.load(stream);
+        }
+
+        props.remove("test0001", "Value0001");
+
+        // Will not remove as value doesn't match
+        props.remove("test0002", "InvalidValue");
+
+        final Iterator<Object> iter = props.keySet().iterator();
+
+        assertEquals("test0002", iter.next());
+        assertEquals("test0003", iter.next());
+        assertEquals("test0004", iter.next());
+        assertEquals("test0005", iter.next());
+        assertEquals("test0006", iter.next());
+        assertEquals("test0007", iter.next());
+        assertEquals("test0008", iter.next());
+        assertEquals("test0009", iter.next());
+        assertEquals("test0010", iter.next());
+
+    }
+
+    @Test
+    void testClear() throws IOException {
+        final Properties props = new OrderedProperties();
+        try (final InputStream stream = OrderedPropertiesTest.class.getResourceAsStream("/test-props.properties")) {
+            props.load(stream);
+        }
+        assertEquals(10, props.keySet().size());
+
+        props.clear();
+
+        assertTrue(props.keySet().isEmpty());
+        assertFalse(props.keys().hasMoreElements());
+    }
+
+    @Test
+    void testEntrySetNotImplemented() throws IOException {
+        final Properties props = new OrderedProperties();
+
+        assertThrows(NotImplementedException.class, () -> props.entrySet());
+    }
 }
